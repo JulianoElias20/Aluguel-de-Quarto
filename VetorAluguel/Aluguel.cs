39962318515008b5ec7208d93ec9977bdb8b438c
Juliano Elias <julianorelias94@hotmail.com>
2024-04-27 01:12:49 -0300

    Aluguel de Quarto, vetor tipo classe

---
+++ VetorAluguel/Aluguel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetorAluguel
+{
+    internal class Aluguel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Aluguel()
+        {
+
+        }
+
+        public Aluguel(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString()
+        {
+            
+
+            StringBuilder sb = new StringBuilder();
+
+           
+            sb.Append(Quarto + ", " + Nome + ", " + Email);
+            return sb.ToString();
+        }
+    }
+}
---
+++ VetorAluguel/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using VetorAluguel;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos quartos serão alugados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Aluguel[] vect = new Aluguel[9];
+
+            for(int i=1; i <=n; i++)
+            {
+                Console.WriteLine("Alguel #"+i);
+                Console.WriteLine("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Email: ");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Digite o número do quarto (0 a 9)");
+                int numero = int.Parse(Console.ReadLine());
+
+                vect[numero] = new Aluguel(nome, email, numero);
+                
+            }
+
+            for(int i=0; i < vect.Length; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(vect[i].ToString());
+                }
+            }
+        }
+    }
+}
